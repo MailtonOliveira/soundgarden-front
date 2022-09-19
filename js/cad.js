efacce497d38176eb7122b0fb4e542a914d0a0a0
jacksonja14 <jackson.oliveira.2015@outlook.com>
2022-09-19 20:13:51 -0300

    a

--- js/cad.js
+++ js/cad.js
@@ -7,26 +7,24 @@ async function cadEvent(){
             "name":"nometeste",
             "poster": "link da imagem",
             "attractions": [
-                "atracoes"
+            "atracoes"
             ],
             "description": "descricao",
             "scheduled": "2022-09-18T22:00:00.000Z",
             "number_tickets": 10
         }; console.log(newEvent)
-           
+        
         
         
         const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
             method: "POST",
             body: JSON.stringify(newEvent),
-            Headers: { 
+            Headers: {
                 "Content-Type": "application/json; charset=UTF-8",
             },
             
-            
         });
         console.log(response)
-
     
     }catch (error){
     console.log("ta dando pau no cadastro " + error)
