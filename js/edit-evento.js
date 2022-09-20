0f55c9bc1521936f838f516067fa30fc96e62f2d
MailtonOliviera <mailton.s.oliveira@gmail.com>
2022-09-20 20:07:31 -0300

    alterando admin-js

--- js/edit-evento.js
+++ js/edit-evento.js
@@ -3,3 +3,4 @@ const inpAtracoes = document.querySelector("#atracoes");
 const inpDescricao = document.querySelector("#descricao");
 const inpData = document.querySelector("#data");
 const inpTickets = document.querySelector("#lotacao");
+const newForm = document.querySelector(".col-6");
