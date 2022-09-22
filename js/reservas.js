6532b017565b7b365ed70b10802518406c9c491a
MailtonOliviera <mailton.s.oliveira@gmail.com>
2022-09-21 21:07:23 -0300

    teste final reservas

---
+++ js/reservas.js
@@ -0,0 +1,41 @@
+
+
+// btEvento.addEventListener("click", listEvents)
+
+async function listReservas(event) {
+    // event.preventDefault();
+    try {
+        const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/bookings", {
+            headers: {
+                "Content-Type": "application/json"
+            }            
+        });
+        const data = await response.json();
+
+        const btReservas = document.querySelector("tbody")
+
+        data.forEach((event) => {
+        const html = `<tr>
+            <th scope="row">1</th>
+            <td>${event.owner_name}</td>
+            <td>${event.owner_email}</td>
+            <td>${event.number_tickets}</td>
+            <td>
+                <a href="editar.html?id=${event._id}" class="btn btn-secondary">editar</a>
+                <a href="excluir.html?id=${event._id}" class="btn btn-danger">excluir</a>
+            </td>
+        </tr>`;
+
+        btReservas.innerHTML += html
+        console.log(html)
+            
+        });   
+
+
+        
+    } catch (error) {
+        console.log("Ta dando pau " + error)
+        
+    }
+} 
+listReservas()
