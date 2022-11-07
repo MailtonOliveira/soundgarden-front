2c74261c797b1fe78285775c95e5fb961825bcf2
Mailton-Oliveira <mailton.s.oliveira@gmail.com>
2022-11-06 22:36:30 -0300

    add: Banner

--- js/index.js
+++ js/index.js
@@ -24,8 +24,47 @@ async function viewEvent() {
         </article>`;
             clIndex.innerHTML += cardIndex
         });
+
+        
+
+
     } catch (error) {
         console.log("ta dando pau no eventos " + error)
     }
+
+    const controls = document.querySelectorAll(".control");
+        let currentItem = 0;
+        const items = document.querySelectorAll(".item");
+        const maxItems = items.length;
+
+        controls.forEach((control) => {
+            control.addEventListener("click", (e) => {
+                isLeft = e.target.classList.contains("arrow-left");
+
+                if (isLeft) {
+                    currentItem -= 1;
+                } else {
+                    currentItem += 1;
+                }
+                
+                if (currentItem >= maxItems) {
+                    currentItem = 0;
+                }
+
+                if (currentItem < 0) {
+                    currentItem = maxItems -1;
+                }
+                
+                items.forEach((item) => item.classList.remove("current-item"));
+                
+                items[currentItem].scrollIntoView({
+                    inline: "center",
+                    behavior: "smooth"
+                });
+
+                items[currentItem].classList.add("current-item");
+                
+            });
+        });
 }
 viewEvent()
